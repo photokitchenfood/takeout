cb603f0fe65f05c43005a81e876261c9338d02ac
Andi Lanuza <andi@photokitchenfood.com>
2026-09-14 21:18:32 +0800

    Initial Commit

---
+++ data/meta.js
@@ -0,0 +1,17 @@
+const CATALOG_META = {
+  "lastUpdated": "2026-09-14",
+  "colors": {
+    "beige-brown": {
+      "count": 60
+    },
+    "silver": {
+      "count": 7
+    },
+    "white": {
+      "count": 24
+    },
+    "clear": {
+      "count": 16
+    }
+  }
+};
